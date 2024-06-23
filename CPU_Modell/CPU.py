d441f50b95fbfa47dfea4254626e24de48dda804
Malik <malik.niederhauser@mygymer.ch>
2024-06-23 14:21:47 +0200

    feat & fix: CPU bugs behoben, ISA abgeändert und erstes Programm geschrieben.

--- CPU_Modell/CPU.py
+++ CPU_Modell/CPU.py
@@ -3,64 +3,93 @@ import byte
 class CPU():
     def __init__(self, mainmemory, controlunit, alunit):
         if not isinstance(mainmemory, RAM.RAM) or not isinstance(controlunit, CU) or not isinstance(alunit, ALU):
-            raise Exception("CPU Error: Arguments given arem't the correct objects.")
+            raise Exception("CPU Error: Arguments given aren't the correct objects.")
         self.mm = mainmemory
         self.cu = controlunit
         self.alu = alunit
 
+    def run(self):
+        while True:
+            self.cu.fetch(self.mm)
+            match self.cu.IR.getByte()[:4]:
+                case "0000":
+                    self.NOP()
+                case "0001":
+                    self.LDA(int(self.cu.IR.getByte()[4:], 2))
+                case "0010":
+                    self.STA(int(self.cu.IR.getByte()[4:], 2))
+                case "0011":
+                    self.ADD(int(self.cu.IR.getByte()[4:], 2))
+                case "0100":
+                    self.SUB(int(self.cu.IR.getByte()[4:], 2))
+                case "0101":
+                    self.JMP(int(self.cu.IR.getByte()[4:], 2))
+                case "0110":
+                    self.JZ(int(self.cu.IR.getByte()[4:], 2))
+                case "0111":
+                    self.JNZ(int(self.cu.IR.getByte()[4:], 2))
+                case "1000":
+                    self.AND(int(self.cu.IR.getByte()[4:], 2))
+                case "1001":
+                    self.OR(int(self.cu.IR.getByte()[4:], 2))
+                case "1010":
+                    break
+                case _:
+                    raise Exception("CPU Error: Unknown Instruction")
+
+
     def NOP(self):
         pass
     def LDA(self, adress):
-        self.alu.accumulator = self.mm[adress]
+        self.alu.accumulator = self.mm.getValueAtIndex
     def STA(self, adress):
-        self.mm[adress] = self.alu.accumulator
+        self.mm.setValueAtIndex(self.alu.accumulator, adress)
     def ADD(self, adress):
-        self.alu.accumulator += self.mm[adress]
+        if not isinstance(self.alu.accumulator, byte.Byte) or not isinstance(self.mm.getValueAtIndex(adress), byte.Byte):
+            raise Exception("Accumulator or adresse in main memory doesn't contain byte")
+        self.alu.accumulator = (self.alu.accumulator).add(self.mm.getValueAtIndex(adress))
     def SUB(self, adress):
-        self.alu.accumulator -= self.mm[adress]
+        if not isinstance(self.alu.accumulator, byte.Byte) or not isinstance(self.mm.getValueAtIndex(adress), byte.Byte):
+            raise Exception("Accumulator or adresse in main memory doesn't contain byte")
+        self.alu.accumulator = (self.alu.accumulator).substract(self.mm.getValueAtIndex(adress))
     def JMP(self, value):
         self.cu.PC = value
     def JZ(self, value):
-        if self.alu.accumulator == 0:   
+        if not isinstance(self.alu.accumulator, byte.Byte):
+            raise Exception("Accumulator doesn't contain byte")
+        if self.alu.accumulator.getByte() == "00000000":
             self.cu.PC = value
     def JNZ(self, value):
-        if self.alu.accumulator != 0:   
+        if not isinstance(self.alu.accumulator, byte.Byte):
+            raise Exception("Accumulator doesn't contain byte")
+        if self.alu.accumulator.getByte() != "00000000":
             self.cu.PC = value
     def AND(self, adress):
-        if not isinstance(self.alu.accumulator, byte.Byte) or not isinstance(self.mm[adress]):
+        if not isinstance(self.alu.accumulator, byte.Byte) or not isinstance(self.mm.getValueAtIndex(adress), byte.Byte):
             raise Exception("Accumulator or adresse in main memory doesn't contain byte")
-        self.alu.accumulator = self.alu.accumulator.bitwise_and(self.mm[adress])
+        self.alu.accumulator = self.alu.accumulator.bitwise_and(self.mm.getValueAtIndex(adress))
     def OR(self, adress):
-        if not isinstance(self.alu.accumulator, byte.Byte) or not isinstance(self.mm[adress], byte.Byte):
+        if not isinstance(self.alu.accumulator, byte.Byte) or not isinstance(self.mm.getValueAtIndex, byte.Byte):
             raise Exception("Accumulator or adresse in main memory doesn't contain byte")
-        self.alu.accumulator = self.alu.accumulator.bitwise_or(self.mm[adress])
+        self.alu.accumulator = self.alu.accumulator.bitwise_or(self.mm.getValueAtIndex(adress))
     def HLT(self):
-        exit()       
+        exit()
 
-class CU(CPU):
+class CU():
     def __init__(self):
-        super().__init__()
         self.PC = 0
-        self.IR = ""
+        self.IR = byte.Byte()
     def reset(self):
         self.PC = 0
-        self.IR = ""
+        self.IR = byte.Byte()
     def fetch(self, memory):
         if not isinstance(memory, RAM.RAM):
             raise Exception("Fetch Error")
-        self.IR = memory[self.PC]
+        self.IR = memory.getValueAtIndex(self.PC)
         self.PC += 1
         
-
         
-class ALU(CPU):
+class ALU():
     def __init__(self):
-        super().__init__()
-        self.mode = 0
-        self.accumulator = 0
-    def calculate(self, value):
-        if self.mode == 0:
-            self.accumulator = value
-        elif self.mode == 1:
-            self.accumulator += value
+        self.accumulator = byte.Byte()
         
--- CPU_Modell/main.py
+++ CPU_Modell/main.py
@@ -1,9 +1,29 @@
 import RAM
 import CPU
 import byte
-"""
-mainmemory = RAM.RAM(1024)
+
+mainmemory = RAM.RAM(16)
 controlunit = CPU.CU()
 arithmeticandlogicunit = CPU.ALU()
-cpu = CPU.CPU(mainmemory, controlunit, arithmeticandlogicunit)"""
+cpu = CPU.CPU(mainmemory, controlunit, arithmeticandlogicunit)
 
+
+
+mainmemory.setValueAtIndex(byte.Byte().setByte('00000001'), 0)
+mainmemory.setValueAtIndex(byte.Byte().setByte('00000010'), 1)
+controlunit.PC = 2
+mainmemory.setValueAtIndex(byte.Byte().setByte('00110000'), 2)
+mainmemory.setValueAtIndex(byte.Byte().setByte('00110001'), 3)
+mainmemory.setValueAtIndex(byte.Byte().setByte('00101111'), 4)
+mainmemory.setValueAtIndex(byte.Byte().setByte('10100000'), 5)
+for value in mainmemory.registers:
+    print(value.getByte())
+
+print("")
+print("")
+print("")
+
+cpu.run()
+
+for value in mainmemory.registers:
+    print((int(value.getByte(), 2)))
