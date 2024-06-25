4de221f9f556705658b688cacbc2c3fb641d60c7
Malik <malik.niederhauser@mygymer.ch>
2024-06-25 16:28:22 +0200

    fix: MOV sollte jetzt funktionieren.

--- CPU_Modell/main.py
+++ CPU_Modell/main.py
@@ -13,25 +13,25 @@ cpu = CPU.CPU(mainmemory, controlunit, arithmeticandlogicunit)
 
 
 
-mainmemory.setValueAtIndex(byte.Byte().setByte('00000111'), 0)
-mainmemory.setValueAtIndex(byte.Byte().setByte('00000010'), 1)
+mainmemory.setValueAtIndex(byte.Byte().setByte('11110000'), 0)
+mainmemory.setValueAtIndex(byte.Byte().setByte('00000000'), 1)
 controlunit.PC = register.Register(byte.Byte().setByte('00000000'), byte.Byte().setByte('00000010'))
 
 # Move x0 to xaaaa
 mainmemory.setValueAtIndex(byte.Byte().setByte('00001011'), 2) # MOV
-mainmemory.setValueAtIndex(byte.Byte().setByte('00000000'), 3) # Operand 1 (x6)1 (aaf1)
-mainmemory.setValueAtIndex(byte.Byte().setByte('00000110'), 4) # Operand 2 (x6)2 (aaf2)
+mainmemory.setValueAtIndex(byte.Byte().setByte('00000000'), 3) # Address big to start index
+mainmemory.setValueAtIndex(byte.Byte().setByte('01100100'), 4) # Address small to start index
 mainmemory.setValueAtIndex(byte.Byte().setByte('00001010'), 5) # HLT
 
-mainmemory.setValueAtIndex(byte.Byte().setByte('00000000'), 6) # x0 big (af1)
-mainmemory.setValueAtIndex(byte.Byte().setByte('00000000'), 7) # x0 small (af2)
-mainmemory.setValueAtIndex(byte.Byte().setByte('10101010'), 8) # xaaaa big (at1)
-mainmemory.setValueAtIndex(byte.Byte().setByte('10101010'), 9) # xaaaa small (at2)
+mainmemory.setValueAtIndex(byte.Byte().setByte('00000000'), 100) # Adress big to the value we want to move (start index)
+mainmemory.setValueAtIndex(byte.Byte().setByte('00000000'), 101) # Adress small to the value we want to move
+mainmemory.setValueAtIndex(byte.Byte().setByte('11111111'), 102) # Address big to the destination
+mainmemory.setValueAtIndex(byte.Byte().setByte('11111111'), 103) # Address small to the destination
 
 cpu.run()
 
 k = 0
 for value in mainmemory.registers:
-    if value.getByte() != "00000000" and PRINT_RAM:
+    if (value.getByte() != "00000000" and PRINT_RAM) or k < 10:
         print(hex(k), value.getByte())
     k += 1
