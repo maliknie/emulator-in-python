2fbaeaac15ef4f7f550e8c69adc8f97a7e9fcabf
Malik <malik.niederhauser@mygymer.ch>
2024-06-28 18:08:57 +0200

    feat: Added 16 color palette and color picker function.

--- CPU_V1/screen.py
+++ CPU_V1/screen.py
@@ -2,6 +2,44 @@ import pygame
 import sys
 import random
 
+def color_picker(string: str):
+    match string:
+        case "0000":
+            return (0, 0, 0)
+        case "0001":
+            return (157, 157, 157)
+        case "0010":
+            return (255, 255, 255)
+        case "0011":
+            return (190, 38, 51)
+        case "0100":
+            return (224, 111, 139)
+        case "0101":
+            return (73, 60, 43)
+        case "0110":
+            return (164, 100, 34)
+        case "0111":
+            return (235, 137, 49)
+        case "1000":
+            return (247, 226, 107)
+        case "1001":
+            return (47, 72, 78)
+        case "1010":
+            return (68, 137, 26)
+        case "1011":
+            return (163, 206, 39)
+        case "1100":
+            return (27, 38, 50)
+        case "1101":
+            return (0, 87, 132)
+        case "1110":
+            return (49, 162, 242)
+        case "1111":
+            return (178, 220, 239)
+        case _:
+            print("Color out of bounds (color_picker)")
+            return (255, 0, 255)
+
 class pygameDisplay:
     def __init__(self, ram):
         self.ram = ram
@@ -11,24 +49,8 @@ class pygameDisplay:
         color_bytes = self.ram.registers[63488:] # letzten 2 kb 
         
         for i, color_byte in enumerate(color_bytes):
-            match color_byte.getByte()[:4]:
-                case "0000":
-                    first_color = (0, 0, 0)  
-                case "1111":
-                    first_color = (255, 255, 255)  
-                case _:
-                    first_color = (221, 0, 255)  
-
-            match color_byte.getByte()[4:]:
-                case "0000":
-                    second_color = (0, 0, 0) 
-                case "1111":
-                    second_color = (255, 255, 255)
-                case _:
-                    second_color = (221, 0, 255) 
-
-            self.colors[i*2] = first_color
-            self.colors[i*2+1] = second_color
+            self.colors[i*2] = color_picker(color_byte.getByte()[:4])
+            self.colors[i*2+1] = color_picker(color_byte.getByte()[4:])
 
 
     def run(self):
@@ -39,6 +61,7 @@ class pygameDisplay:
         draw_surface = pygame.Surface(logical_size)
         screen = pygame.display.set_mode(display_size)
         clock = pygame.time.Clock()
+        pygame.display.set_caption('Pixelbased Display')
         
         running = True
         while running:
